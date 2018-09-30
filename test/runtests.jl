d2097ba2cf908ef2dde427c8496827c2cb8738f0
Christopher Waas <43119886+chriswaas@users.noreply.github.com>
2018-09-30 17:46:11 -0400

    Test cases added

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,15 +6,17 @@ tol = 1e-14
 for i in 1:100000
   x = rand(-1000000:1000000)
   y = rand(1:1000000)
+  t = rand(2:20)
   z = x//y
   appro = getApproxSeries(z,tol)
   @test abs(z-getApprox(z,tol)) <= tol
   if length(appro) > 1
     @test abs(z-getApproxSeries(z,tol)[end-1]) >= tol
   end
+  @test abs(abs(z)^(1//t)-getApprox(abs(z)^(1//t),tol)) <= tol
 end
 
 #testing e and pi, to be perfected
-@test abs(log(getApprox(exp(1),tol))) <= 1.001
-@test abs(cos(getApprox(pi/2, tol))) <= 0.001
+@test  isapprox(abs(log(getApprox(exp(1),tol))), 1)
+@test  isapprox(abs(cos(getApprox(pi/2, tol))), 0, atol = 1e-14)
 end
