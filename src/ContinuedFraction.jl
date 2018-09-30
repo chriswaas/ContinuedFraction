88520005b8ffc90fa0930b57ce9fe177e41538fd
Christopher Waas <43119886+chriswaas@users.noreply.github.com>
2018-09-30 16:42:22 -0400

    Initial commit

---
+++ src/ContinuedFraction.jl
@@ -0,0 +1,60 @@
+module ContinuedFraction
+
+function evaluate(x::Number, a::Array{Int64,1}, iter::Int64)
+    for j = iter-1:-1:1
+        x = 1//x+a[j]
+    end
+    return x
+end
+
+function generate(x::Number, tol::Number)
+    counter = 1
+    z = 0
+    a = zeros(Int64, 100)
+    b = zeros(Number, 100)
+    konv = zeros(Rational, 100)
+    a[1] = Int64(floor(x))
+    b[1] = x
+    konv[1] = a[1]
+    if abs(konv[1]-x)<=tol
+        z = konv[1]
+    else
+        for i in 2:length(a)
+            try
+                b[i] = 1/(b[i-1]-a[i-1])
+                a[i] = Int64(floor(b[i]))
+                konv[i] = evaluate(a[i],a,i)
+            catch
+                b[i] = 0
+                a[i] = Int64(floor(b[i]))
+                konv[i] = evaluate(a[i],a,i)
+            end
+            counter = i
+            if abs(konv[i]-x)<=tol
+                z = konv[i]
+                break
+            end
+        end
+    end
+    return z,a,konv,counter
+end
+
+export getApprox
+function getApprox(x::Number, tol::Number)
+    return generate(x,tol)[1]
+end
+
+export getApproxSeries
+function getApproxSeries(x::Number, tol::Number)
+    i = 2
+    d = generate(x,tol)[3]
+    while d[i] != 0
+        i += 1
+    end
+    val = zeros(Rational, i-1)
+    for j in 1:i-1
+        val[j] = d[j]
+    end
+    return val
+end
+end
---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using ContinuedFraction
+using Test
+
+@testset "Testing rational approximation" begin
+tol = 1e-14
+for i in 1:1000
+  x = rand(-1000000:1000000)
+  y = rand(1:1000000)
+  z = x//y
+  appro = getApproxSeries(z,tol)
+  @test abs(z-getApprox(z,tol)) <= tol
+  if length(appro) > 1
+    @test abs(z-getApproxSeries(z,tol)[end-1]) >= tol
+  end
+end
+
+#testing e and pi
+@test abs(log(getApprox(exp(1),tol))) <= 1.001
+@test abs(cos(getApprox(pi/2, tol))) <= 0.001
+end
