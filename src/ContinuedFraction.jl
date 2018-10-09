16628b38eefabd1dc1a32f04c00ef12d44c0cd6d
Christopher Waas <43119886+chriswaas@users.noreply.github.com>
2018-10-09 18:38:15 -0400

    Finished readme, changed name of functions

--- src/ContinuedFraction.jl
+++ src/ContinuedFraction.jl
@@ -37,47 +37,41 @@ end
 
 function generate(x::Number, tol::Number)
     counter = 1
-    #z = 0
     store1 = zeros(Int, 100) #stores coefficients
-    store2 = zeros(Number, 100) #stores remaining number of x
+    store2 = zeros(Number, 100) #stores remaining part of x
     konv = zeros(Rational, 100) #stores convergents
     store1[1] = Int(floor(x)) #initialize
     store2[1] = x
     konv[1] = store1[1]
-    if abs(konv[1]-x) <= tol #if floor of x already approximates good enough, stop here
+    if abs(konv[1]-x) <= tol #if floor of x already approximates good enough, stop here (e.g. integer approximation)
         z = konv[1]
     else
         for i in 2:length(store1)
-            counter = i
-            try #calculation of the next konvergent
-                dif = (store2[i-1]-store1[i-1])
-                if dif != 0
-                    store2[i] = 1/dif
-                    store1[i] = Int(floor(store2[i]))
-                    konv[i] = evaluate(store1, i) #evaluateRec(shorten(store1), 1) also works but I think it's slower
-                    if abs(konv[i]-x) <= tol #check if fraction approximates x good enough, then break
-                        z = konv[i]
-                        break
-                    end
-                else
+        counter = i
+            dif = (store2[i-1]-store1[i-1])
+            if dif != 0
+                store2[i] = 1/dif #algorithm of wikipedia
+                store1[i] = Int(floor(store2[i]))
+                konv[i] =  evaluate(store1, i) #evaluateRec(shorten(store1), 1) also works but I think it's slower
+                if abs(konv[i]-x) <= tol #check if fraction approximates x good enough, then break
+                    z = konv[i]
                     break
                 end
-            catch
+            else
                 break
             end
-            #println(store1[i],store2[i]) #debug
         end
     end
     return z,store1,konv,counter,tol
 end
 
-export getApprox #returns fraction that approximates x within tol
-function getApprox(x::Number, tol::Number)
+export approximate #returns fraction that approximates x within tol
+function approximate(x::Number, tol::Number)
     return generate(x,tol)[1]
 end
 
-export getApproxSeries #returns all convergents & tolerance
-function getApproxSeries(x::Number, tol::Number)
+export approximateSeries #returns all convergents & tolerance
+function approximateSeries(x::Number, tol::Number)
     i = 2
     d = generate(x,tol)[3]
     while d[i] != 0
@@ -96,7 +90,7 @@ end
 
 export getDeviation #returns deviation of each convergent to x
 function getDeviation(x::Number, tol::Number)
-    y = getApproxSeries(x,tol)
+    y = approximateSeries(x,tol)
     println("Needed steps: ", length(y[1]))
     return pl
 end
